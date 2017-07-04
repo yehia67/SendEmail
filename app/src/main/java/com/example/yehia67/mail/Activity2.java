44c896b0262a64d940254152efbf54342192513b
yehia67 <yehia67@gmail.com>
2017-07-04 17:21:09 +0200

    make Exception if the  user don't have any email app.Start Designing the app.

--- app/src/main/java/com/example/yehia67/mail/Activity2.java
+++ app/src/main/java/com/example/yehia67/mail/Activity2.java
@@ -1,11 +1,14 @@
 package com.example.yehia67.mail;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity implements View.OnClickListener {
 
@@ -26,6 +29,17 @@ public class Activity2 extends AppCompatActivity implements View.OnClickListener
     }
     @Override
     public void onClick(View view) {
+     Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));//only email apps should handle this
+        intent.putExtra(Intent.EXTRA_TEXT,msgcontent);
+        intent.putExtra(Intent.EXTRA_SUBJECT,"bargb el app bta3y");
+        intent.putExtra(Intent.EXTRA_EMAIL,new String[]{"15p6013@eng.asu.edu.eg"});
+            try {
+                startActivity(intent);
+            } catch (Exception e){
+                 e.printStackTrace();
+                Toast.makeText(this,"there is no email apps in your phone.",Toast.LENGTH_SHORT).show();
+            }
 
-    }
+            }
 }
