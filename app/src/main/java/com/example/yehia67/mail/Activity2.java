b7e74639d2a5b614a5ff6bed5e8a1a40f6f79430
yehia67 <yehia67@gmail.com>
2017-07-04 16:17:04 +0200

    finish the design and the code of activity2 now we have the first 2 screen ready to use and the next step is to use SendEmail button to send the msg to all users

--- app/src/main/java/com/example/yehia67/mail/Activity2.java
+++ app/src/main/java/com/example/yehia67/mail/Activity2.java
@@ -1,8 +1,31 @@
 package com.example.yehia67.mail;
 
-/**
- * Created by yehia67 on 03/07/17.
- */
+import android.os.Bundle;
+import android.support.v4.widget.TextViewCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Activity2 {
+public class Activity2 extends AppCompatActivity implements View.OnClickListener {
+
+    String msgcontent;
+    TextView tvreq;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+        Bundle b = getIntent().getExtras();
+        msgcontent = b.getString("Extra key","default");// in case no data in Intent print default
+        tvreq = (TextView) findViewById(R.id.tv);
+        tvreq.setText(msgcontent);
+
+        btn = (Button)findViewById(R.id.btnEmail);
+        btn.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+
+    }
 }
